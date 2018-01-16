4ab3b083a4fb4e2304ca0fbe4ebde75f090503e4
479833357 <479833357@qq.com>
2018-01-16 21:15:48 +0800

    final

--- ShiYanLouHomeWork/FouristHomeWork/two-Json-read/datafigure.py
+++ ShiYanLouHomeWork/FouristHomeWork/two-Json-read/datafigure.py
@@ -7,29 +7,31 @@ import numpy as np
 
 class StudentLearn():
     def analysis(self):
-        self.minutes_dict = self.StudentData[['user_id','minutes']].groupby('user_id').sum()
-        return self.minutes_dict
-    
+        self.minutes_dict = self.StudentData.groupby('user_id').sum()
     def __init__(self):
          try:
-            self.StudentData = pd.read_json(user_study.json)
+            self.StudentData = pd.read_json('user_study.json')
          except ValueError:
             return None
-
     def draw(self):
           fig = plt.figure()
           ax = fig.add_subplot(1,1,1)
-
           ax.set_title("StudyData")
-
           id_major_ticks = np.arange(0,224000,4000)
           id_minor_ticks = np.arange(0,224000,500)
           minu_major_ticks = np.arange(0,500,50)
           minu_minor_ticks = np.arange(0,500,5)
           ax.set_xticks(id_major_ticks)
-          ax.set_xticks(id_minor_ticks)
+          ax.set_xticks(id_minor_ticks,minor=True)
           ax.set_yticks(minu_major_ticks)
-          ax.set_yticks(minu_major_ticks)
-
+          ax.set_yticks(minu_minor_ticks,minor=True)
           ax.set_ylabel('Study Time')
           ax.set_xlabel('User ID')
+          self.minutes_dict.plot()
+          plt.show()
+
+if __name__ == '__main__':
+    a = StudentLearn()
+    a.analysis()
+    a.draw()
+    
